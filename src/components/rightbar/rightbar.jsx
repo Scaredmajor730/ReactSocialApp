d2ffd6986a2c8bcfa56bb6455279aef5e2b06789
Mitul Kumar <mitulkumardayal@gmail.com>
2023-08-18 13:20:58 +0530

    4th commit

--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -5,11 +5,23 @@ const Rightbar = () => {
         <div className='rightbar'>
             <div className='rightbarwrapper'>
             <div className='birthdaycontainer'>
-            <img className='birthdayimg' src="" alt=""/>
+            <img className='birthdayimg' src="../assets/birthday.jpg" alt=""/>
             <span className='birthdaytext'>
-            {" "} Rohan and 5 others have birthday today
+            {" "} <b>Rohan</b> and 5 others have birthday today
             </span>
             </div>
+            <img className='rightbarad' src="../assets/ad.png" alt=""/>
+            <h4 className='rightbartitle'>Online friends</h4>
+             <ul className='rightbarfriendlist'>
+              <li className='rightbarfriend'>
+                 <div className='rightbarprofileimgcontainer'>
+                  <img className='rightbarprofileimg' src='../assets/profile picture/dp5.jpg' alt=''/>
+                  <span className='rightbaronline'></span>
+                 </div>
+                 <span className='rightbarusername'>Leslie Winkle</span>
+                
+              </li>
+             </ul>
             </div>
         </div>
     );
